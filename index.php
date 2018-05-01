a2b4c2e6439b31f67f3069e6d042882a7a9cec56
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-01 15:47:39 +0200

    create filter

--- index.php
+++ index.php
@@ -2,12 +2,20 @@
 
 	require_once 'vendor/autoload.php';
 
+	// ------------------ config -----------------------------
 	$loader = new Twig_Loader_Filesystem('views');
 	// In development set 'cache' => false so it will be reloaded every time u change a template
 	// In production set 'cache' => 'cache' so it will cache templates in cache folder
 	$twig = new Twig_Environment($loader, array(
 		'cache' => false
 	));
+
+	// Filter note: this should be the first config
+	$md5filter = new Twig_SimpleFilter('md5', function($string){
+		return md5($string);
+	});
+
+	$twig->addFilter($md5filter);
 
 	// Default tags u can change them as u like
 	$lexer = new Twig_Lexer($twig, array(
@@ -16,6 +24,8 @@
 	));
 
 	$twig->setLexer($lexer);
+
+	// -------------------------------------------------------
 
 	echo $twig->render('index.html', array(
 		'name' => 'Mahdy',
